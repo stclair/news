3aa2a66a1152e7db5bb6a7dc1e4749c782cb4461
Edward <wes.stclair@theimtgroup.com>
2012-03-11 20:52:06 -0500

    Replaced default context processors.

--- settings/defaults.py
+++ settings/defaults.py
@@ -162,5 +162,10 @@ LOGGING = {
     }
 }
 
-TEMPLATE_CONTEXT_PROCESSORS = ('madisonian.context.mad_sections',
-                               'django.contrib.auth.context_processors.auth',)
+TEMPLATE_CONTEXT_PROCESSORS = ("madisonian.context.mad_sections",
+                               "django.contrib.auth.context_processors.auth",
+                               "django.core.context_processors.debug",
+                               "django.core.context_processors.i18n",
+                               "django.core.context_processors.media",
+                               "django.core.context_processors.static",
+                               "django.contrib.messages.context_processors.messages")
